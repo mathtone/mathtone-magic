94d81b7b28fc2e110f41db43ce20c3a386f7cace
mik3c <mike.carleton@gmail.com>
2022-09-14 20:56:02 -0400

    fix maybe?

--- src/tests/Sandbox.Tests/SqlConnectionExtensionTests.cs
+++ src/tests/Sandbox.Tests/SqlConnectionExtensionTests.cs
@@ -9,7 +9,8 @@ using System.Threading.Tasks;
 namespace Sandbox.Tests {
 	public class SqlConnectionExtensionTests {
 
-		string SQL_PWD = "bqlgd59VylnV";
+		string SQL_PWD = "test!1234";
+		string PGSQL_PWD = "test1234";
 
 		[Fact]
 		public void TestSqlConnection() {
@@ -21,14 +22,14 @@ namespace Sandbox.Tests {
 
 		[Fact]
 		public void TestNpgsqlConnection() {
-			var cn = new NpgsqlConnection($"Server=localhost:5432;User Id=postgres;Password={SQL_PWD}");
+			var cn = new NpgsqlConnection($"Server=localhost:5432;User Id=postgres;Password={PGSQL_PWD}");
 			cn.Open();
 			cn.Close();
 			Assert.True(true);
 		}
 	}
 }
-//docker run --name mathtone-test-pgsql -e POSTGRES_PASSWORD=bqlgd59VylnV  -d -p 5432:5432 postgres
+//docker run --name mathtone-test-pgsql -e POSTGRES_PASSWORD=test1234  -d -p 5432:5432 postgres
 
-//docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=bqlgd59VylnV"  -p 1433:1433 --name mathtone-test-sql --hostname sql1   -d   mcr.microsoft.com/mssql/server:2022-latest
+//docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=test!1234"  -p 1433:1433 --name mathtone-test-sql --hostname sql1   -d   mcr.microsoft.com/mssql/server:2022-latest
 //docker run --name pgsql1 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=test!1234 -e PGDATA=/var/lib/postgresql/data/pgdata -v /tmp:/var/lib/postgresql/data -p 5432:5432 -it postgres
