b98a508165e1f4f999f3e012235ec64fc2927f2e
mik3c <mike.carleton@gmail.com>
2022-09-14 20:14:48 -0400

    change cfg to run sql/pgsql tests

---
+++ src/tests/Sandbox.Tests/SqlConnectionExtensionTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.Tests {
+	public class SqlConnectionExtensionTests {
+
+		[Fact]
+		public void TestConnection() {
+			var cn = new SqlConnection("Server=localhost;User Id=sa;Password=test!1234");
+			cn.Open();
+			cn.Close();
+			;
+		}
+	}
+}
