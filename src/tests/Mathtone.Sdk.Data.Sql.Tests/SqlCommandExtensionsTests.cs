bb511f89c631ecd6ee911345d3ff9fc9e91ee53a
mik3c <mike.carleton@gmail.com>
2022-09-16 17:43:39 -0400

    Done.

---
+++ src/tests/Mathtone.Sdk.Data.Sql.Tests/SqlCommandExtensionsTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+
+namespace Mathtone.Sdk.Data.Sql.Tests {
+	public class SqlCommandExtensionsTests {
+
+		[Fact]
+		public void CreateCommand_Returns_Command() {
+			var cmd = new SqlCommand();
+			var now = DateTimeOffset.Now;
+			cmd.WithInput("@Test", now, SqlDbType.DateTimeOffset);
+			var p = cmd.Parameters["@Test"];
+			Assert.Equal(SqlDbType.DateTimeOffset, p.SqlDbType);
+			Assert.Equal(now, p.Value);
+		}
+	}
+}
---
+++ src/tests/Mathtone.Sdk.Data.Sql.Tests/SqlDataReaderExtensionsTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.SqlClient;
+
+namespace Mathtone.Sdk.Data.Sql.Tests {
+	public class SqlDataReaderExtensionsTests {
+		[Fact]
+		public async Task ConsumeAsync_1() {
+			using var cn = CreateConnection();
+			cn.Open();
+			var rslt = await (await cn.CreateCommand("SELECT value FROM STRING_SPLIT('1 2 3', ' ');")
+				.ExecuteReaderAsync())
+				.ConsumeAsync(r => Task.Run(()=>r.Field<int>(0)))
+				.ToArrayAsync();
+
+
+			Assert.Equal(3, rslt.Length);
+		}
+
+		[Fact]
+		public async Task ConsumeAsync_2() {
+			using var cn = CreateConnection();
+			cn.Open();
+			var rslt = await (await cn.CreateCommand("SELECT value FROM STRING_SPLIT('1 2 3', ' ');")
+				.ExecuteReaderAsync())
+				.ConsumeAsync(r => r.Field<int>(0))
+				.ToArrayAsync();
+			Assert.Equal(3, rslt.Length);
+		}
+
+		protected SqlConnection CreateConnection() => CreateConnection(($"Server=localhost;User Id=sa;Password=test!1234"));
+
+		protected SqlConnection CreateConnection(string connectionString) => new(connectionString);
+	}
+}
