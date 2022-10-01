6d738e05ef5c5750300e94bd96292fec44df1b44
mik3c <mike.carleton@gmail.com>
2022-10-01 19:35:07 -0400

    don't clear locals

--- tools/build-util/Program.cs
+++ tools/build-util/Program.cs
@@ -116,6 +116,7 @@ namespace Build_Util {
 
 			for (var i = 0; i < genCommands.Count; i++) {
 				_genCommand.AppendLine($"echo \"**********GENERATION {i}\"");
+
 				_genCommand.AppendLine(string.Join($" {Environment.NewLine}", genCommands[i]));
 				_genCommand.AppendLine($"ls -l {_config.PackageDirectory}");
 			}
