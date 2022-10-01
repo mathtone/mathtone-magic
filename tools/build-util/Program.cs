30d4c5c4e901fdceb5bd7ed17cd61d8dca29cc83
mik3c <mike.carleton@gmail.com>
2022-10-01 19:28:02 -0400

    typo.

--- tools/build-util/Program.cs
+++ tools/build-util/Program.cs
@@ -97,7 +97,7 @@ namespace Build_Util {
 					}
 					genCmd.Add($"echo \"***** {pj.Project.ProjectName}\"");
 					genCmd.Add($"echo \"-   restoring {pj.Project.ProjectName}\"");
-					genCmd.Add($"dotnet restore {pj.Project.AbsolutePath} -s `local --verbosity {(int)_config.Verbosity}");
+					genCmd.Add($"dotnet restore {pj.Project.AbsolutePath} -s local --verbosity {(int)_config.Verbosity}");
 
 					if (pack) {
 						genCmd.Add($"echo");
