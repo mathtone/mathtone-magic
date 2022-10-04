af4dd193603d62e5f3007e71166184f8324cc25c
mik3c <mike.carleton@gmail.com>
2022-10-03 21:43:37 -0400

    Add license.

--- src/sdk/Mathtone.Sdk.Testing.Xunit/XunitLogger.cs
+++ src/sdk/Mathtone.Sdk.Testing.Xunit/XunitLogger.cs
@@ -1,10 +1,5 @@
 ﻿using Mathtone.Sdk.Logging;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit.Abstractions;
 
 namespace Mathtone.Sdk.Testing.Xunit {
