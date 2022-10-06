fee659b2bc4daf8588b857f3570ad5f027ad25f9
mik3c <mike.carleton@gmail.com>
2022-10-06 05:09:26 -0400

    Release/1.0.5 beta (#19)
    * Added dirty coverage.
    
    * Added logging to Xunit Test Base.
    
    * Added wait handle extensions.

---
+++ src/sdk/Mathtone.Sdk.Common/Extensions/WaitHandleExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Mathtone.Sdk.Common.Extensions {
+	public static class WaitHandleExtensions {
+
+		public static Task WaitOneAsync(this WaitHandle waitHandle) {
+			if (waitHandle == null)
+				throw new ArgumentNullException(nameof(waitHandle));
+
+			var tcs = new TaskCompletionSource<bool>();
+			var rwh = ThreadPool.RegisterWaitForSingleObject(waitHandle, (_, _) => tcs.TrySetResult(true), null, -1, true);
+			var t = tcs.Task;
+			tcs.Task.ContinueWith(_ => rwh.Unregister(null));
+			return t;
+		}
+	}
+}
