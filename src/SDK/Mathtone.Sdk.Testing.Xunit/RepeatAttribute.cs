82b933335ba6fa1ee34e100d279fca7bcb5da0a7
mik3c <mike.carleton@gmail.com>
2022-10-09 06:32:00 -0400

    hmm.

--- src/SDK/Mathtone.Sdk.Testing.Xunit/RepeatAttribute.cs
+++ src/SDK/Mathtone.Sdk.Testing.Xunit/RepeatAttribute.cs
@@ -2,23 +2,23 @@
 using Xunit.Sdk;
 
 namespace Mathtone.Sdk.Testing.Xunit {
-	public sealed class RepeatAttribute : DataAttribute {
-		private readonly int count;
+	//public sealed class RepeatAttribute : DataAttribute {
+	//	private readonly int count;
 
-		public RepeatAttribute(int count) {
-			if (count < 1) {
-				throw new System.ArgumentOutOfRangeException(
-					paramName: nameof(count),
-					message: "Repeat count must be greater than 0."
-					);
-			}
-			this.count = count;
-		}
+	//	public RepeatAttribute(int count) {
+	//		if (count < 1) {
+	//			throw new System.ArgumentOutOfRangeException(
+	//				paramName: nameof(count),
+	//				message: "Repeat count must be greater than 0."
+	//				);
+	//		}
+	//		this.count = count;
+	//	}
 
-		public override IEnumerable<object[]> GetData(MethodInfo testMethod) {
-			foreach (var I in Enumerable.Range(1, count)) {
-				yield return new object[] { I };
-			}
-		}
-	}
+	//	public override IEnumerable<object[]> GetData(MethodInfo testMethod) {
+	//		foreach (var I in Enumerable.Range(1, count)) {
+	//			yield return new object[] { I };
+	//		}
+	//	}
+	//}
 }
