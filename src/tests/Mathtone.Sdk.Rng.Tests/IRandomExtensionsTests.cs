9d2f701a317ce8fd30abd65e69dc52cf6a4e19a7
mik3c <mike.carleton@gmail.com>
2022-10-09 16:49:47 -0400

    Re-add rng tests.

--- src/tests/Mathtone.Sdk.Rng.Tests/IRandomExtensionsTests.cs
+++ src/tests/Mathtone.Sdk.Rng.Tests/IRandomExtensionsTests.cs
@@ -38,22 +38,22 @@ namespace Mathtone.Sdk.Rng.Tests {
 			Assert.Equal(new[] { 3, 1, 2, 0, 4 }, r);
 		}
 
-		//[Theory]
-		//[InlineData(AlphaNumInclude.All, 20, "-lX%A_1Tzl-NBK=J%+Cc")]
-		//[InlineData(AlphaNumInclude.Lower, 20, "zerxjyspjdzojmzmxzja")]
-		//[InlineData(AlphaNumInclude.Upper, 20, "ZERXJYSPJDZOJMZMXZJA")]
-		//[InlineData(AlphaNumInclude.Number, 20, "02704087420645059041")]
-		//[InlineData(AlphaNumInclude.Symbol, 20, "+@_=%+_*%@+&%^+^=+%!")]
-		//[InlineData(AlphaNumInclude.AlphaNum, 20, "9jP4v7SLvj8HxE9D40xc")]
-		//[InlineData(AlphaNumInclude.AlphaNumUpper, 20, "9FY7M9ZVMF9TNR0R70NB")]
-		//[InlineData(AlphaNumInclude.AlphaNumLower, 20, "9fy7m9zvmf9tnr0r70nb")]
-		//[InlineData(AlphaNumInclude.All, 200, "-lX%A_1Tzl-NBK=J%+CchC%-L!^VmTh7LKPh#NI3uKyRU@Ab^Gc7v@qRtZDLceG3dmc$*$TBMV5SdwyVJ$33RxKWu=0mj_ZpeSOI0Jgc9WtWhTN-A5bly1k2XCS-dHrQ_OOH4#9a8Z^&e!OXG#8B*O$zH97H#j=BFZZOegpJ_bAU8pX90*MnW#UDXp+*gAkX2NLAk+Sz")]
-		//public void Random_GetString(AlphaNumInclude include,int count, string expected) {
-		//	var actual = _rng.GetString(count, include);
-		//	Output.WriteLine(actual);
-		//	Assert.Equal(count, actual.Length);
-		//	Assert.Equal(expected, actual);
-		//}
+		[Theory]
+		[InlineData(AlphaNumInclude.All, 20, "-lX%A_1Tzl-NBK=J%+Cc")]
+		[InlineData(AlphaNumInclude.Lower, 20, "zerxjyspjdzojmzmxzja")]
+		[InlineData(AlphaNumInclude.Upper, 20, "ZERXJYSPJDZOJMZMXZJA")]
+		[InlineData(AlphaNumInclude.Number, 20, "02704087420645059041")]
+		[InlineData(AlphaNumInclude.Symbol, 20, "+@_=%+_*%@+&%^+^=+%!")]
+		[InlineData(AlphaNumInclude.AlphaNum, 20, "9jP4v7SLvj8HxE9D40xc")]
+		[InlineData(AlphaNumInclude.AlphaNumUpper, 20, "9FY7M9ZVMF9TNR0R70NB")]
+		[InlineData(AlphaNumInclude.AlphaNumLower, 20, "9fy7m9zvmf9tnr0r70nb")]
+		[InlineData(AlphaNumInclude.All, 200, "-lX%A_1Tzl-NBK=J%+CchC%-L!^VmTh7LKPh#NI3uKyRU@Ab^Gc7v@qRtZDLceG3dmc$*$TBMV5SdwyVJ$33RxKWu=0mj_ZpeSOI0Jgc9WtWhTN-A5bly1k2XCS-dHrQ_OOH4#9a8Z^&e!OXG#8B*O$zH97H#j=BFZZOegpJ_bAU8pX90*MnW#UDXp+*gAkX2NLAk+Sz")]
+		public void Random_GetString(AlphaNumInclude include, int count, string expected) {
+			var actual = _rng.GetString(count, include);
+			Output.WriteLine(actual);
+			Assert.Equal(count, actual.Length);
+			Assert.Equal(expected, actual);
+		}
 
 		[Theory]
 		[InlineData(64, "GlYo8DawrVb2LHLjJnkddgn6J6Y1WbbOIhK6wh8fZJN6i6zCdRTF0AVqF3wTdK1hyvFyMR8ZusGfWc4j9us31g==")]
