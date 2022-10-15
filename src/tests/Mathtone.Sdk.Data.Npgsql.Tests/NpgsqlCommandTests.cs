1b7010ef468822b8150b7812205e6bccf51d5766
mik3c <mike.carleton@gmail.com>
2022-10-15 00:08:18 -0400

    Added npgsql.

--- src/tests/Mathtone.Sdk.Data.Npgsql.Tests/NpgsqlCommandTests.cs
+++ src/tests/Mathtone.Sdk.Data.Npgsql.Tests/NpgsqlCommandTests.cs
@@ -1,16 +1,25 @@
 ﻿using Mathtone.Sdk.Data.Tests;
 using Npgsql;
+using NpgsqlTypes;
 using System.Data;
 using System.Data.Common;
-using System.Data.SqlClient;
 using Xunit.Abstractions;
 
 namespace Mathtone.Sdk.Data.Npgsql.Tests {
 
-	public class NpgsqlCommandTests : DbCommandTestBase<NpgsqlConnection, SqlCommand> {
+	public class NpgsqlCommandTests : DbCommandTestBase<NpgsqlConnection, NpgsqlCommand>  {
 
 		public NpgsqlCommandTests(ITestOutputHelper output) : base(output) { }
 
 		protected override NpgsqlConnection Connect() => new(DB.ConnectionString);
+
+		[Fact]
+		public void WithInput_SqlDbType() {
+			var p = Connect().CreateCommand("").WithInput("@id", 1, NpgsqlDbType.Integer).Parameters["@id"];
+			Assert.Equal(1, p.Value);
+			Assert.Equal(ParameterDirection.Input, p.Direction);
+		}
+
+	
 	}
 }
