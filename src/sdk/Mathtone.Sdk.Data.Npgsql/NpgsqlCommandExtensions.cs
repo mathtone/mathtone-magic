429279007f05e5369ab527c37b7a65771a043f37
mik3c <mike.carleton@gmail.com>
2022-10-15 00:10:58 -0400

    Release/1.0.7 alpha (#26)
    * added some async.
    
    * Added npgsql.

---
+++ src/sdk/Mathtone.Sdk.Data.Npgsql/NpgsqlCommandExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Mathtone.Sdk.Data;
+using Mathtone.Sdk.Data.Npgsql;
+using Npgsql;
+using NpgsqlTypes;
+using System.Data;
+
+
+namespace Mathtone.Sdk.Data.Npgsql {
+	public static class NpgsqlCommandExtensions {
+
+		public static NpgsqlCommand WithParameter<T>(this NpgsqlCommand command, string name, T value, ParameterDirection direction, NpgsqlDbType type, int size = default) {
+			var p = command.CreateParameter();
+			p.ParameterName = name;
+			p.Value = value;
+			p.Direction = direction;
+			p.Size = size;
+			p.NpgsqlDbType = type;			
+			return command.WithParameter(p);
+		}
+
+		public static NpgsqlCommand WithInput<T>(this NpgsqlCommand command, string name, T value, NpgsqlDbType type, int size = default) =>
+			command.WithParameter(name, value, ParameterDirection.Input, type, size);
+
+		//public static SqlCommand WithOutput(this SqlCommand command, string name, SqlDbType type = default, int size = default) =>
+		//	command.WithOutput<object>(name, type, size);
+
+		//public static SqlCommand WithOutput<T>(this SqlCommand command, string name, SqlDbType type = default, int size = default) =>
+		//	command.WithParameter(name, default(T), ParameterDirection.Output, type, size);
+
+		//public static SqlCommand WithInputOutput<T>(this SqlCommand command, string name, T value, SqlDbType type, int size = default) =>
+		//	command.WithParameter(name, value, ParameterDirection.InputOutput, type, size);
+	}
+}
---
+++ src/sdk/Mathtone.Sdk.Data.Npgsql/NpgsqlConnectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Mathtone.Sdk.Data;
+using System.Data;
+using Npgsql;
+
+namespace Mathtone.Sdk.Data.Npgsql {
+
+	public static class NpgsqlConnectionExtensions {
+		public static NpgsqlCommand CreateCommand(this NpgsqlConnection connection, string commandText, CommandType type = CommandType.Text, int timeout = 30) =>
+			 connection.CreateCommand<NpgsqlCommand>(commandText, type, timeout);
+	}
+}
