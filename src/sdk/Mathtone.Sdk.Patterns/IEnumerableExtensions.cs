94d3d3c677cc9cccf2fb4e1480e9b177d7202b5d
mik3c <mike.carleton@gmail.com>
2022-10-20 01:08:34 -0400

    Release/1.0.7 (#30)
    * Added Repository patterns.
    
    * Added IdentifiedBase
    
    * Changed to IDictionary.
    
    * Fix IRepo.

--- src/sdk/Mathtone.Sdk.Patterns/IEnumerableExtensions.cs
+++ src/sdk/Mathtone.Sdk.Patterns/IEnumerableExtensions.cs
@@ -1,8 +1,7 @@
 ﻿namespace Mathtone.Sdk.Patterns {
 	public static class IEnumerableExtensions {
 
-
-		public static DictionaryRepository<ID, ITEM> ToRepo<ID, ITEM>(this IEnumerable<ITEM> items, Func<ITEM, ID> idSelector)
+		public static IListRepository<ID, ITEM> ToRepo<ID, ITEM>(this IEnumerable<ITEM> items, Func<ITEM, ID> idSelector) 
 			where ID : notnull {
 
 			var rtn = new DictionaryRepository<ID, ITEM>(idSelector);
