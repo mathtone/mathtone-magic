0f2ac5d06ef53ea703e5ee30cd693609f6bcb1e3
mik3c <mike.carleton@gmail.com>
2022-10-20 01:00:15 -0400

    Release/1.0.7 rc2 (#29)
    * Added Repository patterns.
    
    * Added IdentifiedBase

--- src/sdk/Mathtone.Sdk.Common/Extensions/IIdentified.cs
+++ src/sdk/Mathtone.Sdk.Common/Extensions/IIdentified.cs
@@ -2,4 +2,11 @@
 	public interface IIdentified<out ID> {
 		ID Id { get; }
 	}
+
+	public class Identified<ID> : IIdentified<ID> {
+		public Identified(ID id) {
+			Id = id;
+		}
+		public ID Id { get; }
+	}
 }
