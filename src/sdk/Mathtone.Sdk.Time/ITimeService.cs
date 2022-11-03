f94c4b75e9fa47cab9fb3fb43f2754535736e0bd
mik3c <mike.carleton@gmail.com>
2022-11-03 03:21:24 -0400

    Release/1.0.9 alpha (#34)
    * Added Repository patterns.
    
    * Added IdentifiedBase
    
    * Changed to IDictionary.
    
    * Fix IRepo.
    
    * Made some things virtual.
    
    * added extensions.
    
    * Added Time interfaces.
    
    * Added extensions for time service.

--- src/sdk/Mathtone.Sdk.Time/ITimeService.cs
+++ src/sdk/Mathtone.Sdk.Time/ITimeService.cs
@@ -1,4 +1,7 @@
-﻿namespace Mathtone.Sdk.Time {
+
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.CompilerServices;
+namespace Mathtone.Sdk.Time {
 
 	public interface ITime : ICurrentTime<DateTime> { }
 	public interface ITimeOffset : ICurrentTime<DateTimeOffset> { }
@@ -16,5 +19,13 @@
 	public struct TimeInZone {
 		public DateTimeOffset Time { get; set; }
 		public TimeZoneInfo Zone { get; set; }
+	}
+
+	public static class TimeInZoneExtensions {
+		public static IServiceCollection AddTime<SVC>(this IServiceCollection services)
+			where SVC : class, ITimeService => services
+				.AddSingleton<ITimeService, SVC>()
+				.AddSingleton<ITime>(svc => svc.GetRequiredService<ITimeService>())
+				.AddSingleton<ITimeOffset>(svc => svc.GetRequiredService<ITimeService>());
 	}
 }
