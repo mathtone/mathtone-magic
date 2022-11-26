394cb637bf91379c551890afe6bdd8df74f7b923
mik3c <mike.carleton@gmail.com>
2022-11-26 15:48:53 -0500

    added data record extensions.

--- src/sdk/Mathtone.Sdk.Services/ServiceCollectionExtensions.cs
+++ src/sdk/Mathtone.Sdk.Services/ServiceCollectionExtensions.cs
@@ -33,5 +33,8 @@ namespace Mathtone.Sdk.Services {
 	public static class IServiceProviderExtensions {
 		public static SVC Activate<SVC>(this IServiceProvider services, params object[] parameters) =>
 			ActivatorUtilities.CreateInstance<SVC>(services, parameters);
+
+		public static SVC Activate<SVC, IMPL>(this IServiceProvider services, params object[] parameters) where IMPL : SVC =>
+			services.Activate<IMPL>(parameters);
 	}
 }
