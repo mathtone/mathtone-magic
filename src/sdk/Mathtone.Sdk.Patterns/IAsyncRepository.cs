82b4e1e7451a57b2c5b1149e75348ca97cb5f4a6
mik3c <mike.carleton@gmail.com>
2022-11-26 17:26:41 -0500

    Added async repo pattern.

---
+++ src/sdk/Mathtone.Sdk.Patterns/IAsyncRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Mathtone.Sdk.Patterns {
+	public interface IAsyncRepository<ID, ITEM> {
+		ValueTask<ID> Create(ITEM item);
+		ValueTask<ITEM> Read(ID id);
+		Task Update(ITEM item);
+		Task Delete(ID id);
+	}
+	public interface IAsyncListRepository<ID, ITEM> : IAsyncRepository<ID, ITEM> {
+		IAsyncEnumerable<ITEM> ReadAll();
+	}
+}
+
