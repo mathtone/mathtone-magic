c3828cbb4f6608d03117e8294e16d57191cb87b6
mik3c <mike.carleton@gmail.com>
2022-12-15 07:25:45 -0500

    Added async repository.

--- src/sdk/Mathtone.Sdk.Patterns/DictionaryRepository.cs
+++ src/sdk/Mathtone.Sdk.Patterns/DictionaryRepository.cs
@@ -2,6 +2,34 @@
 
 namespace Mathtone.Sdk.Patterns {
 
+	public class AsyncDictionaryRepositiory<ID, ITEM> : DictionaryRepository<ID, ITEM>, IAsyncListRepository<ID, ITEM> where ID : notnull {
+		public AsyncDictionaryRepositiory(Func<ITEM, ID> idSelector) : base(idSelector) {
+		}
+
+		public AsyncDictionaryRepositiory(Func<ITEM, ID> idSelector, IEnumerable<ITEM> items) : base(idSelector, items) {
+		}
+
+		public AsyncDictionaryRepositiory(Func<ITEM, ID> idSelector, IDictionary<ID, ITEM> items) : base(idSelector, items) {
+		}
+
+		ValueTask<ID> IAsyncRepository<ID, ITEM>.Create(ITEM item) =>new(base.Create(item));
+
+		Task IAsyncRepository<ID, ITEM>.Delete(ID id) => Task.Run(() => base.Delete(id));
+
+		ValueTask<ITEM> IAsyncRepository<ID, ITEM>.Read(ID id) =>new(base.Read(id));
+
+		async IAsyncEnumerable<ITEM> IAsyncListRepository<ID, ITEM>.ReadAll() {
+			foreach (var i in base.ReadAll()) {
+				yield return i;
+			}
+			await Task.CompletedTask;
+		}
+
+		Task IAsyncRepository<ID, ITEM>.Update(ITEM item) {
+			throw new NotImplementedException();
+		}
+	}
+
 	public class DictionaryRepository<ID, ITEM> : IListRepository<ID, ITEM> where ID : notnull {
 
 		public DictionaryRepository(Func<ITEM, ID> idSelector) : this(idSelector, Array.Empty<ITEM>()) { }
