2d4c50b100e7593345d4b06d235cf83af98fc60e
mik3c <mike.carleton@gmail.com>
2022-12-21 10:24:50 -0500

    Addes broadcaster/subscriber.

---
+++ src/sdk/Mathtone.Sdk.Patterns/Broadcaster.cs
@@ -0,0 +1,42 @@
+﻿using Mathtone.Sdk.Common;
+using System.Threading.Channels;
+
+namespace Mathtone.Sdk.Patterns {
+	public class Broadcaster<T> : AsyncDisposableBase {
+
+		public Broadcaster() {
+			_processTask = Task.Run(async () => {
+				await foreach (var i in _channel.Reader.ReadAllAsync()) {
+					await Task.WhenAll(
+						subscribers.Select(s => s.Writer.WriteAsync(i).AsTask())
+					);
+				}
+			});
+		}
+
+		readonly Task _processTask;
+		readonly List<Subscriber<T>> subscribers = new();
+		readonly Channel<T> _channel = Channel.CreateUnbounded<T>();
+
+
+		public ValueTask Send(T item) => _channel.Writer.WriteAsync(item);
+
+		public Subscriber<T> Subscribe() {
+			var rtn = new Subscriber<T>();
+			subscribers.Add(rtn);
+			return rtn;
+		}
+		public void UnSubscribe(Subscriber<T> subscriber) {
+			subscribers.Remove(subscriber);
+		}
+
+		protected override async ValueTask OnDisposeAsync() {
+			await base.OnDisposeAsync();
+			_channel.Writer.TryComplete();
+			await _processTask;
+			await Task.WhenAll(
+				subscribers.Select(s => Task.Run(() => s.Writer.Complete()))
+			);
+		}
+	}
+}
---
+++ src/sdk/Mathtone.Sdk.Patterns/Subscriber.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Channels;
+
+namespace Mathtone.Sdk.Patterns {
+	public class Subscriber<T> {
+		readonly Channel<T> _channel = Channel.CreateUnbounded<T>();
+
+		public ChannelWriter<T> Writer => _channel.Writer;
+
+		public IAsyncEnumerable<T> ReadAllAsync() => _channel.Reader.ReadAllAsync();
+	}
+}
