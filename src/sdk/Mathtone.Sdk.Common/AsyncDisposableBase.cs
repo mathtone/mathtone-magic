58a823f01d842ada8c7662f364bde7e7e2c00ec4
mik3c <mike.carleton@gmail.com>
2022-12-21 10:27:06 -0500

    Addes broadcaster/subscriber. (#48)

---
+++ src/sdk/Mathtone.Sdk.Common/AsyncDisposableBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathtone.Sdk.Common {
+	public class AsyncDisposableBase : IAsyncDisposable {
+
+		public async ValueTask DisposeAsync() {
+			await OnDisposeAsync().ConfigureAwait(false);
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual ValueTask OnDisposeAsync() => ValueTask.CompletedTask;
+	}
+
+	public static class AsyncDisposal {
+		public static Task Of(params IAsyncDisposable[] disposables) {
+			return Task.WhenAll(disposables.Select(d => d.DisposeAsync().AsTask()));
+		}
+	}
+}
---
+++ src/sdk/Mathtone.Sdk.Common/DisposableBase.cs
@@ -0,0 +1,21 @@
+﻿namespace Mathtone.Sdk.Common {
+	public abstract class DisposableBase : IDisposable {
+		private bool disposedValue;
+
+		protected virtual void Dispose(bool disposing) {
+			if (!disposedValue) {
+				if (disposing) {
+					OnDisposing();
+				}
+				disposedValue = true;
+			}
+		}
+		protected virtual void OnDisposing() { }
+
+		public void Dispose() {
+			// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+			Dispose(disposing: true);
+			GC.SuppressFinalize(this);
+		}
+	}
+}
