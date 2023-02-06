d42ad6fdcf51a65b3a9a102390e60beb4a0d01e5
mik3c <mike.carleton@gmail.com>
2023-02-06 01:39:38 -0500

    Release.

---
+++ src/sdk/Mathtone.Sdk.Observables/ObjectExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Calxium.Observable {
+	public static class ObjectExtensions {
+		public static IObservableValue<T> CreateObservable<T>(this T value) {
+			return new ObservableValue<T>(value);
+		}
+	}
+}
---
+++ src/sdk/Mathtone.Sdk.Observables/ObservableValue.cs
@@ -0,0 +1,39 @@
+﻿using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace Calxium.Observable {
+	public struct ObservableValue<T> : IObservableValue<T> {
+
+
+		public ObservableValue(T value) => _value = value;
+
+		T _value;
+		readonly Subject<T> _subject = new();
+
+		public T Value {
+			get => _value;
+			set => _subject.OnNext(_value = value);
+		}
+
+		public IObservable<T> Observe => _subject.AsObservable().Prepend(_value);
+
+
+		public static implicit operator T(ObservableValue<T> value) => value.Value;
+		public static implicit operator ObservableValue<T>(T value) => new(value);
+	}
+
+	public interface IObservableValue<T> : IObservable<T> {
+		T Value { get; set; }
+		IObservable<T> Observe { get; }
+		IDisposable IObservable<T>.Subscribe(IObserver<T> observer) =>
+			Observe.Subscribe(observer);
+	}
+
+	//public class ValueChangedEventArgs<T> : EventArgs {
+	//	public ValueChangedEventArgs(T oldValue, T newValue) =>
+	//		(OldValue, NewValue) = (oldValue, newValue);
+
+	//	public T OldValue { get; }
+	//	public T NewValue { get; }
+	//}
+}
