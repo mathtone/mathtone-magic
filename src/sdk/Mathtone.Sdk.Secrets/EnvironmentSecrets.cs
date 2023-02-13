daf1245939a21cff020aad457f8994476f369c47
mik3c <mike.carleton@gmail.com>
2023-02-13 04:41:31 -0500

    Shenanigans...

---
+++ src/sdk/Mathtone.Sdk.Secrets/EnvironmentSecrets.cs
@@ -0,0 +1,7 @@
+﻿namespace Mathtone.Sdk.Secrets {
+	public class EnvironmentSecrets : ISecrets {
+		public EnvironmentSecrets() { }
+
+		public string GetSecret(string key) => Environment.GetEnvironmentVariable(key)!;
+	}
+}
