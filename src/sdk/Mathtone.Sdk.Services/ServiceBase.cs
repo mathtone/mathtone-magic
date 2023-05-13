c933639e6a51e380f2a20e4cc6caba131444cbdc
mik3c <mike.carleton@gmail.com>
2023-05-13 18:16:42 -0400

    Execute tests again.

--- src/sdk/Mathtone.Sdk.Services/ServiceBase.cs
+++ src/sdk/Mathtone.Sdk.Services/ServiceBase.cs
@@ -1,17 +1,30 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Mathtone.Sdk.Services {
 
+	[SuppressMessage("Minor Code Smell", "S2094:Classes should not be empty", Justification = "<Pending>")]
 	public abstract class ServiceBase {
 
 	}
 
 	public abstract class AppServiceBase : ServiceBase {
-		public AppServiceBase(ILogger log)
+		protected ILogger Log;
+		protected AppServiceBase(ILogger log) => Log = log;
+	}
+
+	public abstract class ConfiguredServiceBase<CFG> : AppServiceBase
+		where CFG : class, new() {
+
+		protected ConfiguredServiceBase(ILogger log, CFG config) : base(log) {
+			Config = config;
+		}
+
+		protected CFG Config { get; }
 	}
 }
