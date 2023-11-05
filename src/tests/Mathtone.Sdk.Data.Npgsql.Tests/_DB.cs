a91499c3fa45da4160a51b3aa8e492d78eca73f8
mik3c <mike.carleton@gmail.com>
2023-11-04 20:27:05 -0400

    beta release.

--- src/tests/Mathtone.Sdk.Data.Npgsql.Tests/_DB.cs
+++ src/tests/Mathtone.Sdk.Data.Npgsql.Tests/_DB.cs
@@ -9,8 +9,7 @@ using System.Threading.Tasks;
 namespace Mathtone.Sdk.Data.Npgsql.Tests {
 
 	internal static class DB {
-		//public static readonly string ConnectionString = $"Server=localhost;User Id=postgres;Password={Environment.GetEnvironmentVariable("PGSQL_TEST_PWD")}";
-		public static readonly string ConnectionString = $"Server=localhost;User Id=postgres;Password=test!1234";
+		public static readonly string ConnectionString = $"Server=localhost;User Id=postgres;Password=postgres";
 	}
 
 	[ResourcePath("Queries")]
